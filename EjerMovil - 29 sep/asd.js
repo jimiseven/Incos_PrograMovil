da21034426a376acb2fc85d7f85f1faf80d745e5
LaptopTarks <tano777864@gmail.com>
2023-11-10 08:49:56 -0400

    ejercicio de examen

---
+++ EjerMovil - 29 sep/asd.js
@@ -0,0 +1,9 @@
+INSERT INTO inscripcion (cod_carrera, ci, fecha_inscripcion, encargado, turno) VALUES
+(1, '125', '2015-01-21', 'Luis', 'Nocturno'),
+(1, '126', '2016-01-21', 'Carmen', 'Nocturno'),
+(1, '127', '2017-01-21', 'Luis', 'Nocturno'),
+(1, '128', '2018-01-21', 'Carmen', 'Nocturno'),
+(2, '129', '2015-02-02', 'Luis', 'Nocturno'),
+(2, '130', '2016-02-02', 'Carmen', 'Nocturno'),
+(3, '131', '2017-02-02', 'Luis', 'Nocturno'),
+(3, '132', '2018-02-02', 'Carmen', 'Nocturno');
